6f8d92376bd05f10d6d9a588d2b8450e4ab57fe7
Adri <asri84368@gmail.com>
2020-11-24 21:44:54 +0100

    Database.java - RedWriteLcok with custom monitor that prioretises writers

--- src/cw2020/Database.java
+++ src/cw2020/Database.java
@@ -7,52 +7,168 @@ package cw2020;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  * @author DAVID
  */
 public class Database {
     
+    /*for previous equal priority implementation, retained for review purposes
+    private ReadWriteLock lock;
+    private Lock readLock; //read lock
+    private Lock writeLock; //write lock
+    */
+    
+    //fields to implement monitor that prioretises Writers
+    private ReentrantLock theEntryLock ;
+    private Condition okToRead ;
+    private Condition okToWrite ;
+    private int numReaders ;
+    private int numWriters ;
+    private int numReadersWaiting ;
+    private int numWritersWaiting ;
+    
+    //original fields
     private ArrayList<Person> registered;
     private HashMap<Person, ArrayList<Contact>> contactRecords;
-    
-    private ReadWriteLock lock;
     
     public Database(){
         registered = new ArrayList();
         contactRecords = new HashMap();
+        
+        theEntryLock = new ReentrantLock();
+        okToWrite = theEntryLock.newCondition();
+        okToRead = theEntryLock.newCondition();
+        numReaders = 0;
+        numWriters = 0;
+        numReadersWaiting = 0;
+        numWritersWaiting = 0;
+        
+        /*equal priority implementation
+        //initialise locks
         lock = new ReentrantReadWriteLock();
+        readLock = lock.readLock();
+        writeLock = lock.writeLock();
+*/
     }
     
-    public void registerPhone(Person p){
-        lock.writeLock().lock();
+     //methods for prioretising Writers
+    public void startWrite() throws InterruptedException{
+        try {    
+            theEntryLock.lock();
+            //while (!((numReaders == 0) && (numWriters == 0)) ) {
+            while ((numReaders > 0) || (numWriters > 0) ) {
+                numWritersWaiting++;
+                okToWrite.await();
+                numWritersWaiting--;
+            }
+            numWriters++;
+        } finally {
+            theEntryLock.unlock();
+        }
+    }
+    
+
+    public void endWrite(){
+        try { 
+            theEntryLock.lock();
+            numWriters--;
+            if (numWritersWaiting > 0) {
+                okToWrite.signal();
+            } else {
+                okToRead.signal();
+            }
+        } finally {
+            theEntryLock.unlock();
+        }
+    }    
+    
+
+    public void startRead() throws InterruptedException{
+         try {
+            theEntryLock.lock();
+            //while (!((numWriters == 0) && (numWritersWaiting == 0))) {
+            while ((numWriters > 0) || (numWritersWaiting > 0)) {
+                numReadersWaiting++;
+                okToRead.await();
+                numReadersWaiting--;
+            }
+            numReaders++;
+            okToRead.signal();
+        } finally {
+            theEntryLock.unlock();
+        }
+    }
+
+    public void endRead(){
+        theEntryLock.lock();
         try {
+            numReaders--;
+            if (numReaders == 0) {
+                okToWrite.signal();
+            }
+        } finally {
+            theEntryLock.unlock();
+        }
+    }   
+    
+    public void registerPhone(Person p){
+        
+        try {
+            //start writing by locking the writeLock
+            //writeLock.lock();
+
+            startWrite();
             if(!registered.contains(p)){
                 registered.add(p);
                 contactRecords.put(p, new ArrayList<>());
             }
-        } finally {
-            lock.writeLock().unlock();
+         
+            //end reading by unlocking the write lock
+            //writeLock.unlock();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
+        finally {
+            endWrite();
+        }
+        
     }
     
     public void recordContact(Person p , Contact c){
-        lock.writeLock().lock();
+        //writeLock.lock();
         try {
+            startWrite();
             contactRecords.get(p).add(c);
-        } finally {
-            lock.writeLock().unlock();
+        } 
+        catch (InterruptedException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally {
+            endWrite();
+           // writeLock.unlock();
         }
     }
     
     public boolean isPhoneRegistered(Person p){
-        lock.readLock().lock();
-        boolean isRegistered;
+        //readLock.lock();
+        boolean isRegistered = false;
         try {
+            startRead();
             isRegistered = registered.contains(p);
-        } finally {
-            lock.readLock().unlock();
+        } 
+        catch (InterruptedException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally {
+            //readLock.unlock();
+            endRead();
         }
         return isRegistered;
     }
@@ -78,11 +194,18 @@ public class Database {
 
     public int getNumberContacts() {
         int total = 0;
-        lock.readLock().lock();
+        
+        //readLock.lock();
         try {
+            startRead();
             total = registered.stream().map((p) -> contactRecords.get(p).size()).reduce(total, Integer::sum);
-        } finally {
-            lock.readLock().unlock();
+        } 
+        catch (InterruptedException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally {
+            //readLock.unlock();
+            endRead();
         }
         return total;
     }
